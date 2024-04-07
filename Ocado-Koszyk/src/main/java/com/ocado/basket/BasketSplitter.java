3a01a35b67f8c3779126c75de28e2752b3ca1150
Dominik <263915@student.pwr.edu.pl>
2024-04-07 21:36:30 +0200

    change split algorithm

--- Ocado-Koszyk/src/main/java/com/ocado/basket/BasketSplitter.java
+++ Ocado-Koszyk/src/main/java/com/ocado/basket/BasketSplitter.java
@@ -4,8 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BasketSplitter {
     private Map<String, List<String>> deliveryOptions;
@@ -31,6 +30,117 @@ public class BasketSplitter {
 
     public Map<String, List<String>> split(List<String> items) {
 
+        Map<String, Integer> occurrence = new HashMap<>();
+
+        for (String item : items) {
+            for (String option : deliveryOptions.get(item)) {
+                if (occurrence.containsKey(option)) {
+                    occurrence.put(option, occurrence.get(option)+1);
+                } else {
+                    occurrence.put(option, 1);
+                }
+            }
+        }
+
+
+        List<Map.Entry<String, Integer>> sortedOccurrence = new ArrayList<>(occurrence.entrySet());
+        Collections.sort(sortedOccurrence, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+
+
+        Map<String, List<String>> finalResult = new HashMap<>();
+        List<String> productList = new ArrayList<>();
+
+        List<String> helpItemsList = new ArrayList<>(items);
+
+        while (true) {
+
+            List<Map.Entry<String, Integer>> sortOccurrence = computeOccurrence(helpItemsList);
+            if (sortOccurrence.isEmpty()) {
+                break;
+            }
+            for (Map.Entry<String, Integer> e : sortOccurrence ){
+
+                if (productList.size() == helpItemsList.size()) {
+                    break;
+                } else {
+                    List<String> helpProductList = new ArrayList<>();
+                    List<String> helpItemList2 = new ArrayList<>(helpItemsList);
+                    for (String item : helpItemList2 ) {
+                        if (!productList.contains(item)){
+                            if (deliveryOptions.get(item).contains(e.getKey())){
+                                helpProductList.add(item);
+                                productList.add(item);
+                                helpItemsList.remove(item);
+                            }
+                        }
+                    }
+                    if (!helpProductList.isEmpty()) {
+                        finalResult.put(e.getKey(), helpProductList);
+                    }
+
+                }
+            }
+
+        }
+
+        System.out.println(finalResult);
+
+
+
+//        for (Map.Entry<String, Integer> e : sortedOccurrence ){
+//
+//
+//            if (productList.size() == items.size()) {
+//                break;
+//            } else {
+//                List<String> helpProductList = new ArrayList<>();
+//                for (String item : items ) {
+//                    if (!productList.contains(item)){
+//                        if (deliveryOptions.get(item).contains(e.getKey())){
+//                            helpProductList.add(item);
+//                            productList.add(item);
+//                        }
+//                    }
+//                }
+//                if (!helpProductList.isEmpty()) {
+//                    finalResult.put(e.getKey(), helpProductList);
+//                }
+//
+//            }
+//        }
+//        System.out.println(finalResult);
+
         return null;
     }
+
+    public List<Map.Entry<String, Integer>> computeOccurrence(List<String> items){
+
+        Map<String, Integer> occurrence = new HashMap<>();
+
+        for (String item : items) {
+            for (String option : deliveryOptions.get(item)) {
+                if (occurrence.containsKey(option)) {
+                    occurrence.put(option, occurrence.get(option)+1);
+                } else {
+                    occurrence.put(option, 1);
+                }
+            }
+        }
+
+
+        List<Map.Entry<String, Integer>> sortedOccurrence = new ArrayList<>(occurrence.entrySet());
+        Collections.sort(sortedOccurrence, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+
+        return sortedOccurrence;
+
+    }
+
 }
